ebf12b341e7de9c012b8d458334223049b4c9e7f
Ben-Gendler <bennygendler@gmail.com>
2020-11-10 16:30:25 +0200

    new landing page + grid fixes in registration page added some text values under resources (opened new register_strings.xml file for that)
    TODO:
    finish adding all text values for views as resources and not hardcoded
    attach buttons to the right pages

--- app/src/main/java/com/example/near_buy/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/near_buy/ui/login/LoginActivity.java
@@ -37,10 +37,10 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+        final EditText usernameEditText = findViewById(R.id.Email);
+        final EditText passwordEditText = findViewById(R.id.pass);
+        final Button loginButton = findViewById(R.id.Login_button);
+        final ProgressBar loadingProgressBar = findViewById(R.id.login_progress_bar);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
