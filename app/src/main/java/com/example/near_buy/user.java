b9b67f93072f8555c63e01bc38fa9167e754d995
ahmadml <ahmad_msalha@hotmail.com>
2020-11-23 14:40:21 +0200

    Linking users to the database

---
+++ app/src/main/java/com/example/near_buy/user.java
@@ -0,0 +1,26 @@
+package com.example.near_buy;
+
+public class user {
+    private  String name,id;
+
+    public user(){
+        name = "";
+        id = "";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
