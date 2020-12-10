efd79c7d6630d596f459179bdaea25df54dbf59a
roymash <roymash@gmail.com>
2020-12-10 12:08:51 +0200

    Home fragment - minor fixes

--- app/src/main/java/com/example/near_buy/homeFrag.java
+++ app/src/main/java/com/example/near_buy/homeFrag.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +60,10 @@ public class homeFrag extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container,
+                false);
+
+
+        return rootView;
     }
 }
